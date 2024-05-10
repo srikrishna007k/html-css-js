6a26de98d6de6d4db36d5cf37d0222ae14fdfd05
srikrishna007k <maindev007@gmail.com>
2024-05-11 00:12:14 +0530

    all

---
+++ appounture/app.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector("#btn");
+btn.addEventListener("click",()=>{
+    let ele = document.querySelector("section");
+    ele.classList.toggle("back");
+    
+
+    console.log("hello")
+})
